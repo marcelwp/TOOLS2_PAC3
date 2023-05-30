1370bda906d5433f246fe99cb940104c62377565
Marcel <marcelwp@uoc.edu>
2023-05-31 00:08:00 +0200

    portada i mig responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '450px',
+      }    
+    },
   },
   plugins: [],
 }
