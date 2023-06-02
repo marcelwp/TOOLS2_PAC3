43a30f8a6c9c21c574d8e6b9fe7d0ddf64ee58a8
Marcel <marcelwp@uoc.edu>
2023-06-02 17:34:11 +0200

    apply, posthtml, personalitzar tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
+      colors: {
+        'green': '#bada55',
+        'gray': '#f8f7f3',
+      },
       screens: {
         'sm': '460px',
       }    
